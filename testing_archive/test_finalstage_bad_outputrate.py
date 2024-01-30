18e67f7a1d60e6204aa7b985d3e322d37716e654
Lozzy-Bear <adam.lozinsky@usask.ca>
2024-01-30 17:12:27 +0000

    Updated import path to decimation_scheme

--- testing_archive/test_finalstage_bad_outputrate.py
+++ testing_archive/test_finalstage_bad_outputrate.py
@@ -10,7 +10,7 @@ Expected exception:
 
 import borealis_experiments.superdarn_common_fields as scf
 from experiment_prototype.experiment_prototype import ExperimentPrototype
-from experiment_prototype.decimation_scheme.decimation_scheme import \
+from experiment_prototype.experiment_utils.decimation_scheme import \
     DecimationScheme, DecimationStage, create_firwin_filter_by_attenuation
 
 class TestExperiment(ExperimentPrototype):
