c33aec589778423f0f9e8402e9fbc5a2dd7b2fd7
doreban <doreban@gmail.com>
2024-03-01 13:54:22 -0600

    added slice capability to request retuning of the N200

--- multifreq_tuning.py
+++ multifreq_tuning.py
@@ -14,10 +14,10 @@ from experiment_prototype.experiment_prototype import ExperimentPrototype
 import borealis_experiments.superdarn_common_fields as scf
 
 
-class Twofsound(ExperimentPrototype):
+class TuningSound(ExperimentPrototype):
 
     def __init__(self, **kwargs):
-        cpid = 404
+        cpid = 404  # Tune not found
 
         if scf.IS_FORWARD_RADAR:
             beams_to_use = scf.STD_16_FORWARD_BEAM_ORDER
@@ -30,12 +30,13 @@ class Twofsound(ExperimentPrototype):
             num_ranges = scf.STD_NUM_RANGES
 
         tx_freq_1 = scf.COMMON_MODE_FREQ_1
+        tx_freq_2 = scf.COMMON_MODE_FREQ_2
 
         if kwargs:
-            if 'freq' in kwargs.keys():
-                tx_freq = int(kwargs['freq'])
-        
-        print(f"Tx = {tx_freq_1}")
+            if 'freq1' in kwargs.keys():
+                tx_freq_1 = int(kwargs['freq1'])
+            if 'freq2' in kwargs.keys():
+                tx_freq_2 = int(kwargs['freq2'])
 
         slice_1 = {  # slice_id = 0, the first slice
             "pulse_sequence": scf.SEQUENCE_7P,
@@ -48,18 +49,23 @@ class Twofsound(ExperimentPrototype):
             "rx_beam_order": beams_to_use,
             "tx_beam_order": beams_to_use,
             "scanbound": scf.easy_scanbound(scf.INTT_7P, beams_to_use),
-            "freq": tx_freq,     # kHz
+            "freq": tx_freq_1,     # kHz
             "acf": True,
             "xcf": True,  # cross-correlation processing
             "acfint": True,  # interferometer acfs
+            "tuning_freq": [tx_freq_1, tx_freq_1]
         }
 
-        rxctrfreq = txctrfreq = tx_freq
+        slice_2 = copy.deepcopy(slice_1)
+        slice_2['freq'] = tx_freq_2
+        slice_2['tuning_freq'] = [tx_freq_2, tx_freq_2]
 
+        rxctrfreq = txctrfreq = tx_freq_1
 
         super().__init__(cpid, txctrfreq=txctrfreq, rxctrfreq=rxctrfreq,
-                lo_lock_wait=True, comment_string='Twofsound classic scan-by-scan')
+                         comment_string='A re-tuning (large freq diff) twofsound style experiment')
 
         self.add_slice(slice_1)
+        self.add_slice(slice_2, interfacing_dict={0: 'SCAN'})
 
 
