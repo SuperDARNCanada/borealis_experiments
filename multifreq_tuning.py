b787b05fdfc1ddae972837b1002b81e83d34072f
Draven <doreban@gmail.com>
2024-04-09 15:42:45 +0000

    Another test

--- multifreq_tuning.py
+++ multifreq_tuning.py
@@ -1,47 +1,97 @@
-#!/usr/bin/python
-
 """
-    twofsound
-    ~~~~~~~~~
-    Standard operating Borealis experiment. Alternates transmitting in two different frequencies.
+    full_fov
+    ~~~~~~~~
+    The mode transmits with a pre-calculated phase progression across the array which illuminates
+    the full FOV, and receives on all antennas. The first pulse in each sequence starts on the 0.1
+    second boundaries, to enable bistatic listening on other radars.
 
-    :copyright: 2023 SuperDARN Canada
+    :copyright: 2022 SuperDARN Canada
+    :author: Remington Rohel
+
+    :Draven added retuning for testing of the borelalis retuning capability
 """
 
-import copy
+import numpy as np
+import copy 
+from experiment_prototype.experiment_utils.sample_building import get_phase_shift
 
-from experiment_prototype.experiment_prototype import ExperimentPrototype
 import borealis_experiments.superdarn_common_fields as scf
+from experiment_prototype.experiment_prototype import ExperimentPrototype
 
 
-class TuningTest(ExperimentPrototype):
+def rx_phase_pattern(beam_angle, freq_khz, antenna_count, antenna_spacing, offset=0.0):
+    window = [0.08081232549588463, 0.12098514265395757, 0.23455777475180511, 0.4018918165398586,
+              0.594054435182454, 0.7778186328978896, 0.9214100134552521, 1.0,
+              1.0, 0.9214100134552521, 0.7778186328978896, 0.594054435182454,
+              0.4018918165398586, 0.23455777475180511, 0.12098514265395757, 0.08081232549588463]
 
+    xcf_directions = {
+        10400: [-28.8, -23.96, -19.92, -14.68, -11.24, -7.4, -3.48, -1.36,
+                1.36, 3.48, 7.5, 11.24, 14.68, 19.92, 23.96, 28.8],
+        10500: [-28.8, -23.86, -20.02, -14.78, -11.24, -7.5, -3.53, -1.41,
+                1.41, 3.53, 7.5, 11.24, 14.78, 20.02, 23.86, 29.],
+        10600: [-29., -23.76, -20.02, -14.78, -11.24, -7.5, -3.48, -1.32,
+                1.32, 3.48, 7.6, 11.24, 14.78, 20.02, 23.76, 29.],
+        10700: [-29.1, -23.76, -20.12, -14.88, -11.24, -7.6, -3.53, -1.32,
+                1.32, 3.53, 7.6, 11.24, 14.88, 20.12, 23.76, 29.2],
+        10800: [-29.3, -23.76, -20.12, -14.98, -11.24, -7.6, -3.53, -1.32,
+                1.32, 3.53, 7.7, 11.24, 14.98, 20.12, 23.76, 29.4],
+        10900: [-29.3, -23.66, -20.12, -15.08, -11.24, -7.7, -3.56, -1.32,
+                1.32, 3.56, 7.7, 11.24, 15.08, 20.12, 23.66, 29.4],
+        12200: [-28.4, -22.26, -17.62, -14.78, -11.24, -8.15, -4.96, -1.82,
+                1.82, 4.96, 8.15, 11.24, 14.78, 17.62, 22.26, 28.5],
+        12300: [-28.5, -22.46, -17.62, -14.78, -11.24, -8.1, -4.96, -1.82,
+                1.82, 4.96, 8.1, 11.24, 14.78, 17.62, 22.46, 28.6],
+        12500: [-28.7, -22.56, -17.62, -14.69, -11.24, -7.9, -4.88, -1.92,
+                1.92, 4.88, 7.9, 11.24, 14.69, 17.62, 22.56, 28.8],
+        13000: [-29.3, -22.86, -17.72, -14.58, -11.14, -7.6, -4.96, -2.12,
+                2.12, 4.96, 7.6, 11.14, 14.58, 17.72, 22.86, 29.4],
+        13100: [-29.6, -22.96, -17.82, -14.48, -11.34, -7.55, -4.93, -2.12,
+                2.12, 4.93, 7.55, 11.34, 14.48, 17.82, 22.96, 29.7],
+        13200: [-29.6, -23.06, -17.92, -14.48, -11.24, -7.6, -4.96, -2.12,
+                2.12, 4.96, 7.6, 11.24, 14.48, 17.92, 23.06, 29.7],
+        }
+
+    shift = get_phase_shift(xcf_directions[int(freq_khz)], freq_khz, antenna_count,
+                            antenna_spacing, offset) * 0.9999999
+
+    # Apply a Hamming window to the antenna data streams of the main array
+    if antenna_count == 16:
+        shift = np.einsum('ij,j->ij', shift, np.array(window, dtype=np.float32))
+
+    return shift
+
+
+class FullFOV(ExperimentPrototype):
     def __init__(self, **kwargs):
-        cpid = 3504  # Tune not found
+        """
+        kwargs:
 
-        if scf.IS_FORWARD_RADAR:
-            beams_to_use = scf.STD_16_FORWARD_BEAM_ORDER
-        else:
-            beams_to_use = scf.STD_16_REVERSE_BEAM_ORDER
+        freq: int
 
+        """
+        cpid = 3802
+        super().__init__(cpid)
+
+        num_ranges = scf.STD_NUM_RANGES
         if scf.options.site_id in ["cly", "rkn", "inv"]:
             num_ranges = scf.POLARDARN_NUM_RANGES
-        if scf.options.site_id in ["sas", "pgr", "lab"]:
-            num_ranges = scf.STD_NUM_RANGES
 
-        tx_freq_1 = scf.COMMON_MODE_FREQ_1
-        tx_freq_2 = scf.COMMON_MODE_FREQ_2
-        center_freq_1 = tx_freq_1 - 500
-        center_freq_2 = tx_freq_2 - 500
+        # default frequency set here
+        freq = scf.COMMON_MODE_FREQ_1
 
         if kwargs:
-            if 'freq1' in kwargs.keys():
-                tx_freq_1 = int(kwargs['freq1'])
+            if 'freq' in kwargs.keys():
+                freq = kwargs['freq']
 
-            if 'freq2' in kwargs.keys():
-                tx_freq_2 = int(kwargs['freq2'])
+        txctrfreq_1 = rxctrfreq_1 = freq + 500
+        txctrfreq_2 = rxctrfreq_2 = freq - 500
 
-        slice_1 = {  # slice_id = 0, the first slice
+        print('Frequency set to {}'.format(freq))   # TODO: Log
+
+        num_antennas = scf.options.main_antenna_count
+
+        slice_1 = {  # slice_id = 0, there is only one slice.
             "pulse_sequence": scf.SEQUENCE_7P,
             "tau_spacing": scf.TAU_SPACING_7P,
             "pulse_len": scf.PULSE_LEN_45KM,
@@ -49,23 +99,25 @@ class TuningTest(ExperimentPrototype):
             "first_range": scf.STD_FIRST_RANGE,
             "intt": scf.INTT_7P,  # duration of an integration, in ms
             "beam_angle": scf.STD_16_BEAM_ANGLE,
-            "rx_beam_order": beams_to_use,
-            "tx_beam_order": beams_to_use,
-            "scanbound": scf.easy_scanbound(scf.INTT_7P, beams_to_use),
-            "freq": tx_freq_1,  # kHz
-            "txctrfreq": center_freq_1,
-            "rxctrfreq": center_freq_1,
+            "rx_beam_order": [[i for i in range(num_antennas)]],
+            "tx_beam_order": [0],   # only one pattern
+            "tx_antenna_pattern": scf.easy_widebeam,
+            "rx_antenna_pattern": rx_phase_pattern,
+            "freq": freq,  # kHz
+            "txctrfreq": txctrfreq_1,
+            "rxctrfreq": rxctrfreq_1,
             "acf": True,
             "xcf": True,  # cross-correlation processing
             "acfint": True,  # interferometer acfs
+            "align_sequences": True,     # align start of sequence to tenths of a second
         }
 
         slice_2 = copy.deepcopy(slice_1)
-        slice_2['freq'] = tx_freq_2
-        slice_2['txctrfreq'] = center_freq_2
-        slice_2['rxctrfreq'] = center_freq_2
+        slice_2['txctrfreq'] = txctrfreq_2
+        slice_2['rxctrfreq'] = rxctrfreq_2
 
-        super().__init__(cpid, comment_string='Twofsound classic scan-by-scan')
+        print(f"{txctrfreq_1} {txctrfreq_2} {rxctrfreq_1} {rxctrfreq_2}")
+
 
         self.add_slice(slice_1)
 
