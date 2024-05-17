8a75c4cee7e1d5f2d745322627fa1f7acc4b3e8e
Remington Rohel <rar129@usask.ca>
2024-05-17 20:51:12 +0000

    Minor fix to fix floating point error with widebeam

--- superdarn_common_fields.py
+++ superdarn_common_fields.py
@@ -164,11 +164,11 @@ def easy_widebeam(frequency_khz, tx_antennas, antenna_spacing_m):
     if len(tx_antennas) == 16:
         if frequency_khz in cached_values_16_antennas.keys():
             phases[tx_antennas] = np.exp(1j * np.pi/180. * np.array(cached_values_16_antennas[frequency_khz]))
-            return phases.reshape(1, num_antennas)
+            return phases.reshape(1, num_antennas) * 0.999999
     elif len(tx_antennas) == 8:
         if frequency_khz in cached_values_8_antennas.keys():
             phases[tx_antennas] = np.exp(1j * np.pi/180. * np.array(cached_values_8_antennas[frequency_khz]))
-            return phases.reshape(1, num_antennas)
+            return phases.reshape(1, num_antennas) * 0.999999
     # If you get this far, the number of antennas or frequency is not supported for this function.
     raise ValueError(f"Invalid parameters for easy_widebeam(): tx_antennas: {tx_antennas}, "
                      f"frequency_khz: {frequency_khz}, main_antenna_count: {num_antennas}")
