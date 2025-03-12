3f5f3461525fa4ba5626ab625d86e804508f2043
Remington Rohel <rar129@usask.ca>
2025-03-12 00:47:19 +0000

    Bugfix to the decimation_scheme

--- concurrent_bistatic.py
+++ concurrent_bistatic.py
@@ -89,7 +89,7 @@ class ConcurrentBistatic(ExperimentPrototype):
             "freq": common_freqs.get(scf.options.site_id)[0],
             "scanbound": [i * 3.7 for i in range(len(scf.STD_16_BEAM_ANGLE))],  # align each aveperiod to 3.7s boundary
             "wait_for_first_scanbound": False,
-            "decimation_scheme": two_stage_filter,
+            "decimation_scheme": two_stage_filter(),
             "align_sequences": True,     # align start of sequence to tenths of a second
         }
         slice_1 = None
